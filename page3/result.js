bcb6146782fc549518801dc13d7c657fe0315c5d
abhishek <abiainapur24@gmail.com>
2023-10-12 05:04:16 +0530

    development of result page started

--- page3/result.js
+++ page3/result.js
@@ -4,3 +4,14 @@ localStorage.removeItem('data');
 let userResult = JSON.parse(retrievedObject);
 
 console.log(userResult);
+
+function showSummary()
+{
+    let questionList = userResult.list;
+
+    for(let i = 0; i < questionList.length; i++)
+    {
+        let div = document.createElement("div");
+
+    }
+}
